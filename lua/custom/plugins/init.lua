0deaecb1888a0246fcbb9c34ae409ac78b819747
jacobhm98 <jacobhm98@gmail.com>
2025-06-07 21:10:18 +0200

    test running suite

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,11 +3,13 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  -- provides an lsp and debug capabilities for rust code
   {
     'mrcjkb/rustaceanvim',
     version = '^6', -- Recommended
     lazy = false, -- This plugin is already lazy
   },
+  -- markdown-preview allows rendering markdown files in browser
   {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
@@ -23,6 +25,55 @@ return {
         '<cmd>MarkdownPreviewToggle<cr>',
         ft = 'markdown',
         desc = 'MarkdownPreview: [T]oggle [m]arkdown preview',
+      },
+    },
+  },
+  -- test suite
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'mrcjkb/rustaceanvim', -- Rust testrunner
+    },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require 'rustaceanvim.neotest',
+        },
+      }
+    end,
+    keys = {
+      { '<leader>T', name = '[T]est' },
+      {
+        '<leader>Tn',
+        function()
+          require('neotest').run.run()
+        end,
+        desc = '[T]est [N]earest',
+      },
+      {
+        '<leader>Tf',
+        function()
+          require('neotest').run.run(vim.fn.expand '%')
+        end,
+        desc = '[T]est [F]ile',
+      },
+      {
+        '<leader>To',
+        function()
+          require('neotest').output_panel.toggle()
+        end,
+        desc = '[T]est [O]utput Panel',
+      },
+      {
+        '<leader>Ts',
+        function()
+          require('neotest').summary.toggle()
+        end,
+        desc = '[T]est [S]ummary Panel',
       },
     },
   },
