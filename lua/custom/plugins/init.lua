f43946576a2704de4a9a6169b8b6363a51b3695e
Jacob Heden Malm <jacobhm98@gmail.com>
2025-06-19 08:50:31 +0200

    remove plugin that doesnt work

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -9,25 +9,6 @@ return {
     version = '^6', -- Recommended
     lazy = false, -- This plugin is already lazy
   },
-  -- markdown-preview allows rendering markdown files in browser
-  {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd app && yarn install',
-    init = function()
-      vim.g.mkdp_filetypes = { 'markdown' }
-    end,
-    ft = { 'markdown' },
-
-    keys = {
-      {
-        '<leader>tm',
-        '<cmd>MarkdownPreviewToggle<cr>',
-        ft = 'markdown',
-        desc = 'MarkdownPreview: [T]oggle [m]arkdown preview',
-      },
-    },
-  },
   -- test suite
   {
     'nvim-neotest/neotest',
